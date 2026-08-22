b0c40a15ad3a193b546ec826d5ffb6e189702866
Mohamed Nasr <mohnasr113377@gmail.com>
2026-08-22 11:52:17 +0300

    database connection

--- internal/depend/depend.go
+++ internal/depend/depend.go
@@ -2,9 +2,11 @@ package depend
 
 import (
 	"log"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Application struct {
 	InfoLog *log.Logger
 	ErrorLog *log.Logger
+	DB *pgxpool.Pool
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
@@ -10,36 +11,62 @@ import (
 
 	"snip-it/routes"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/seanzhengw/fileonlyserver"
 )
 
 func main() {
+	// Create loggers
 	infoLog := log.New(os.Stdout, "INFO ", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR ", log.Ldate|log.Ltime|log.Lshortfile)
-	app := &depend.Application{
-		InfoLog:  infoLog,
-		ErrorLog: errorLog,
-	}
 
+	// Load dotenv
 	cfg, errCfg := dotenv.Load()
 	if errCfg != nil {
 		errorLog.Fatal(errCfg)
 	}
+	infoLog.Println("Dotenv loaded successfully")
 
+	// Create database connection pool
+	db, errDB := pgxpool.New(context.Background(), cfg["DATABASE_URL"])
+	if errDB != nil {
+		errorLog.Fatal(errDB)
+	}
+	defer db.Close()
+
+	if err := db.Ping(context.Background()); err != nil {
+		errorLog.Fatal(err)
+	}
+	infoLog.Println("Database connected successfully")
+
+	// Create application
+	app := &depend.Application{
+		InfoLog:  infoLog,
+		ErrorLog: errorLog,
+		DB:       db,
+	}
+
+	// Create router
 	mux := http.NewServeMux()
 
+	// Serve static files
 	fileServer := fileonlyserver.Serve(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Register routes
 	routes.Home(app, mux)
 	routes.Snippet(app, mux)
 
+	// Create HTTP server
 	srv := &http.Server{
 		Addr:     cfg["PORT"],
 		ErrorLog: errorLog,
 		Handler:  mux,
 	}
 
-	infoLog.Printf("Starting server on: http://localhost%s", cfg["PORT"])
+	infoLog.Printf("Starting on: http://localhost%s", cfg["PORT"])
+
+	// Start server
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
